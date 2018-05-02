6d4a3cc1f589ece43c3eb3e13f8e40b20b9e8cf7
Tuyển Nguyễn <Tuyn Nguyn>
2018-05-02 15:27:46 +0700

    update package.json

--- app-user.js
+++ app-user.js
@@ -210,7 +210,7 @@ app.use(function (req, res, next) {
 
 
 // Home page
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
     res.send("Car Map API<br>API for user: /api-user/v1/xxx");
 })
 

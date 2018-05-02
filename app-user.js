af539b97578cdf41a8c3baeedec0bee7ef7e7f38
Tuyển Nguyễn <Tuyn Nguyn>
2018-05-02 12:08:58 +0700

    only mLab run heroku

--- app-user.js
+++ app-user.js
@@ -13,88 +13,88 @@ var Report = require('./models/report');
 // Set up express app
 var app = express();
 
-////////////////// LOCAL /////////////////////
-// Connect to mongo db
-// 1. Drop old database
-mongoose.connect(config.database);
-mongoose.connection.once('open', function () {
-    mongoose.connection.db.dropDatabase(function (err, result) {
-    });
-});
-// 2. Connect again
-mongoose.connect(config.database);
-mongoose.connection.once('open', function () {
-    // Create sample data for database 
-    var user = new User({
-        email: 'user1@gmail.com',
-        password: '123456',
-        name: 'Tuyen',
-        birthDate: "1996-12-13",
-        googleUserID: '11111111',
-        avatar: 'avatar.url',
-        homeLocation: {
-            type: 'Point',
-            // must follow longitude, latitude order
-            coordinates: [105.005675, 10.367511]
-        }
-    });
-    user.save(function (err, user) {
-        if (err) return console.error(err);
-        console.log(user);
-    });
+// ////////////////// LOCAL /////////////////////
+// // Connect to mongo db
+// // 1. Drop old database
+// mongoose.connect(config.database);
+// mongoose.connection.once('open', function () {
+//     mongoose.connection.db.dropDatabase(function (err, result) {
+//     });
+// });
+// // 2. Connect again
+// mongoose.connect(config.database);
+// mongoose.connection.once('open', function () {
+//     // Create sample data for database 
+//     var user = new User({
+//         email: 'user1@gmail.com',
+//         password: '123456',
+//         name: 'Tuyen',
+//         birthDate: "1996-12-13",
+//         googleUserID: '11111111',
+//         avatar: 'avatar.url',
+//         homeLocation: {
+//             type: 'Point',
+//             // must follow longitude, latitude order
+//             coordinates: [105.005675, 10.367511]
+//         }
+//     });
+//     user.save(function (err, user) {
+//         if (err) return console.error(err);
+//         console.log(user);
+//     });
 
-    var car = new Car({
-        type: 1,
-        lastestGeo: {
-            type: "Point",
-            coordinates: [105.045705, 21.345696]
-        },
-        currentGeo: {
-            type: "Point",
-            coordinates: [105.078940, 21.312346]
-        },
-        speed: 80,
-        userID: user._id
-    });
-    car.save(function (err, car) {
-        if (err) return console.error(err);
-        console.log(car);
-    });
+//     var car = new Car({
+//         type: 1,
+//         lastestGeo: {
+//             type: "Point",
+//             coordinates: [105.045705, 21.345696]
+//         },
+//         currentGeo: {
+//             type: "Point",
+//             coordinates: [105.078940, 21.312346]
+//         },
+//         speed: 80,
+//         userID: user._id
+//     });
+//     car.save(function (err, car) {
+//         if (err) return console.error(err);
+//         console.log(car);
+//     });
 
-    var location = new Location({
-        geomatry: {
-            type: "Point",
-            coordinates: [105.045705, 21.345696]
-        },
-        name: 'Home',
-        userID: user._id
-    });
-    location.save(function (err, location) {
-        if (err) return console.error(err);
-        console.log(location);
-    });
+//     var location = new Location({
+//         geomatry: {
+//             type: "Point",
+//             coordinates: [105.045705, 21.345696]
+//         },
+//         name: 'Home',
+//         userID: user._id
+//     });
+//     location.save(function (err, location) {
+//         if (err) return console.error(err);
+//         console.log(location);
+//     });
 
-    var report = new Report({
-        type: 1,
-        desciption: 'Trafic jam',
-        geomatry: {
-            type: "Point",
-            coordinates: [105.045705, 21.345696]
-        },
-        severity: 3,
-        userID: user._id
-    });
-    report.save(function (err, report) {
-        if (err) return console.error(err);
-        console.log(report);
-    });
+//     var report = new Report({
+//         type: 1,
+//         desciption: 'Trafic jam',
+//         geomatry: {
+//             type: "Point",
+//             coordinates: [105.045705, 21.345696]
+//         },
+//         severity: 3,
+//         userID: user._id
+//     });
+//     report.save(function (err, report) {
+//         if (err) return console.error(err);
+//         console.log(report);
+//     });
 
-    console.log('Connect to database successfuly');
-}).on('error', function () {
-    console.log('Conmect to database failed')
-});
+//     console.log('Connect to database successfuly');
+// }).on('error', function () {
+//     console.log('Conmect to database failed')
+// });
 
-///////////////////// END LOCAL ///////////////////
+// ///////////////////// END LOCAL ///////////////////
 
 ///////////////////// MLAB - HEROKU ///////////////
 // Only run first time to create data for mLab collections

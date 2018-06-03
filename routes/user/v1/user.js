1be3950cfdf4d14ef544f866b3a75b919068e821
Tuyển Nguyễn <trongtuyen96@gmail.com>
2018-06-03 18:01:41 +0700

    Add update socketID

--- routes/user/v1/user.js
+++ routes/user/v1/user.js
@@ -74,6 +74,34 @@ router.put("/updateHomeLocation", authenticate, (req, res, next) => {
         }).catch(next);
 })
 
+// Update socket ID
+router.put("/updateSocketID", authenticate, (req, res, next) => {
+    let userID = req.decoded.userID;
+    if (!userID) {
+        return res.status(422).send({
+            success: false,
+            message: "Không thể truy cập userID"
+        });
+    }
+
+    User
+        .findByIdAndUpdate(userID, {
+            socketID : req.body.socketID
+        })
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Người dùng không tồn tại"
+                });
+            }
+            return res.status(200).send({
+                success: true,
+                user: user
+            });
+        }).catch(next);
+})
+
 // Get car of user 
 router.get("/car", authenticate, (req, res, next) => {
     let userID = req.decoded.userID;
